97c7893b036da0b60587f73bbfc453105062e076
Anusha Betha <130354867+Anushabetha14@users.noreply.github.com>
2025-02-21 22:56:08 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,16 +1,12 @@
 from flask import Flask, render_template
-import webbrowser
-import threading
+import os
 
 app = Flask(__name__)
 
 @app.route('/')
 def registration_form():
-    return render_template('index.html')  # Ensure 'index.html' is in the 'templates' folder
-
-def open_browser():
-    webbrowser.open_new('http://127.0.0.1:5000/')
+    return render_template('index.html')  # Ensure 'index.html' is inside 'templates' folder
 
 if __name__ == '__main__':
-    threading.Timer(1.25, open_browser).start()  # Open browser after 1.25 seconds
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))  # Get port dynamically for deployment
+    app.run(host='0.0.0.0', port=port)
